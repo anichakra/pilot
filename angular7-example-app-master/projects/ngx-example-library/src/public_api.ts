b6c2d9a7a368019389c34dc74545ea20004d7354
Anirban Chakraborty <anirbanchakraborty@Anirbans-MacBook-Pro.local>
2019-06-18 09:21:34 +0530

    deleting angular apps

--- angular7-example-app-master/projects/ngx-example-library/src/public_api.ts
+++
@@ -1,7 +0,0 @@
-/*
- * Public API Surface of ngx-example-library
- */
-
-export * from './lib/ngx-example-library.service';
-export * from './lib/ngx-example-library.component';
-export * from './lib/ngx-example-library.module';
